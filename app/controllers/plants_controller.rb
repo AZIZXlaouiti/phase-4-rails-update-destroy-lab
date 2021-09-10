3e5c73705a6ad45f902433d18cc6da04bfd47e5e
AZIZXlaouiti <azizrocke@gmail.com>
2021-09-10 10:21:28 -0400

    completed

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,38 @@
 class PlantsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /plants
   def index
-    plants = Plant.all
-    render json: plants
+    @plants = Plant.all
+    render json: @plants
   end
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    @plant = find_plant
+    render json: @plant
   end
 
   # POST /plants
   def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    @plant = Plant.create(plant_params)
+    render json: @plant, status: :created
   end
-
+  def destroy
+    @plant = find_plant
+    @plant.destroy
+    head :no_content
+  end
+  def update
+     @plant = find_plant
+     @plant.update(is_in_stock: false)
+     render json: @plant
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  def find_plant
+   Plant.find_by(id: params[:id])
   end
 end
